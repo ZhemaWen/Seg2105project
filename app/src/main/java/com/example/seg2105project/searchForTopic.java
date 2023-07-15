6289fef69bd10c6103dd53a2ea0c8f7a6bdde192
Molly-k <64807621+Molly-k@users.noreply.github.com>
2023-07-14 21:03:36 -0400

    Layout for the rating option

---
+++ app/src/main/java/com/example/seg2105project/searchForTopic.java
@@ -0,0 +1,13 @@
+package com.example.seg2105project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class searchForTopic {
+    /*public List<Topic> searchTopics(Student student){
+
+        List<Topic> availableTopic = new ArrayList<>();
+
+
+    }*/
+}
